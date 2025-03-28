72045c8ae9953121f0e0d16b2a01fc3e8d2fa79f
Abhijeet <abhijeetsinha43@gmail.com>
2025-03-28 13:21:40 +0530

    setup: add tailwindcss 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import "./App.css";
+import './App.css';
 
 function App() {
-  return <h1>Zen UI Comming Soon...</h1>;
+  return (
+    <h1 className="font-bold text-purple-500 text-2xl text-center">
+      Zen UI Comming Soon...
+    </h1>
+  );
 }
 
 export default App;
